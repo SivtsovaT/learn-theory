034135a7abed87438375e0317dafd953e852270a
user <sivtsovatanya1985@gmail.com>
2022-02-06 15:10:06 +0200

    Created project learn-theory
    - updated deploy.yml

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  versionName: '1.0.2'
+  versionName: '1.0.2',
+  firebase: {
+    apiKey: "AIzaSyCtB0PQkp4Q_XFkpedUov5Cx5zzOUNGnS4",
+    authDomain: "easy-f7fa7.firebaseapp.com",
+    databaseURL: "https://easy-f7fa7-default-rtdb.firebaseio.com",
+    projectId: "easy-f7fa7",
+    storageBucket: "easy-f7fa7.appspot.com",
+    messagingSenderId: "641737163457",
+    appId: "1:641737163457:web:2ab0a560c6182582378a40",
+    measurementId: "G-EB92Z67KP3"
+  },
 };

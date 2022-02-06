7bd2466c7037bd777b6ce64334f84cf33397b087
user <sivtsovatanya1985@gmail.com>
2022-02-06 14:54:37 +0200

    Created project learn-theory
    - updated deploy.yml

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {environment} from "../environments/environment";
 import {Auth, signInWithEmailAndPassword, createUserWithEmailAndPassword} from "@angular/fire/auth";
+import {Firestore, collectionData, collection} from "@angular/fire/firestore";
+import {Observable} from "rxjs";
+import firebase from "firebase/compat";
+import {DocumentData} from "rxfire/firestore/interfaces";
 
 
 @Component({
@@ -11,14 +15,18 @@ import {Auth, signInWithEmailAndPassword, createUserWithEmailAndPassword} from "
 export class AppComponent {
   title = 'learn-theory';
   version = environment.versionName;
+  users$: Observable<DocumentData[]> | undefined;
 
-  constructor(auth: Auth) {
-   /* createUserWithEmailAndPassword(auth, 'mytest@test.com', 'qwerty')
+  constructor(auth: Auth, firestore: Firestore) {
+    /*createUserWithEmailAndPassword(auth, 'mytest@test.com', 'qwerty')
       .then(user => console.log(user))
       .catch(error => console.error(error));*/
 
     signInWithEmailAndPassword(auth, 'mytest@test.com', 'qwerty')
       .then(user => console.log(user))
+      .then(user => collection(firestore,'users'))
+      .then(users => collectionData(users))
+      .then(data => this.users$ = data)
       .catch(error => console.error(error));
   }
 
